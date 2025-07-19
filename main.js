97f068fe605560b0ddb3f48dacc6d0b79f0feee4
Dariel <58573644+DarielDR21@users.noreply.github.com>
2025-07-19 16:04:38 -0400

    Final Version

---
+++ main.js
@@ -0,0 +1,72 @@
+const removeBtn = document.querySelectorAll('.remove');
+const toggle = document.querySelectorAll('.toggle')
+const activeLink = document.getElementById('active');
+const inactiveLink = document.getElementById('inactive');
+const allLink = document.getElementById('all');
+const filterLinks = document.querySelectorAll('.filters a');
+const extensions = document.querySelectorAll('.extension');
+
+
+filterLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        filterLinks.forEach(l => l.classList.remove('active'));
+        link.classList.add('active');
+
+
+        if (link.id === 'active') {
+            extensions.forEach(extension => {
+                if (extension.classList.contains('favorite')){
+                    extension.style.display = '';
+
+                } else {
+                    extension.style.display = 'none';
+                }
+            });
+        } else if (link.id === 'inactive') {
+            extensions.forEach(extension => {
+                if (!extension.classList.contains('favorite')){
+                    extension.style.display = '';
+                } else {
+                    extension.style.display = 'none';
+                }
+            });
+
+            } else if (link.id === 'all') {
+                extensions.forEach(extension => {
+                    extension.style.display = '';
+                });
+            }
+    });
+});
+
+removeBtn.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const extensionContainer = e.target.closest('.extension');
+
+        if (extensionContainer) {
+            extensionContainer.remove();
+        }
+    })
+})
+
+
+toggle.forEach(toggle => {
+    toggle.addEventListener('click', (t) => {
+        if (toggle.textContent === '⭒') {
+            toggle.textContent = '⭑';
+            const extensionContainer = t.target.closest('.extension');
+            if (extensionContainer) {
+                extensionContainer.classList.add('favorite');
+            }
+        } else {
+            toggle.textContent = '⭒';
+            const extensionContainer = t.target.closest('.extension');
+            if (extensionContainer) {
+            extensionContainer.classList.remove('favorite');
+            }
+        }
+
+    });
+});
+
